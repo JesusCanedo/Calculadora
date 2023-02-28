8cb686e114c2d28c1a18b469beee41e3fde72bc7
Jesus <jesus.canedo228508@potros.itson.edu.mx>
2023-02-28 16:23:10 -0600

    Se cambiaron los metodos por uno solo llamado operar

--- app/src/main/java/mx/itson/calculadora/MainActivity.java
+++ app/src/main/java/mx/itson/calculadora/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText txtOp1;
     private EditText txtOp2;
 
-//Funciona
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,66 +29,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txtOp1 = findViewById(R.id.txtOp1);
         txtOp2 = findViewById(R.id.txtOp2);
 
-        Button btnSumar = (Button)findViewById(R.id.btnSumar);
+        Button btnSumar = (Button) findViewById(R.id.btnSumar);
         btnSumar.setOnClickListener(this);
 
-        Button btnRestar = (Button)findViewById(R.id.btnRestar);
+        Button btnRestar = (Button) findViewById(R.id.btnRestar);
         btnRestar.setOnClickListener(this);
 
-        Button btnMultiplicar = (Button)findViewById(R.id.btnMultiplicar);
+        Button btnMultiplicar = (Button) findViewById(R.id.btnMultiplicar);
         btnMultiplicar.setOnClickListener(this);
 
-        Button btnDividir = (Button)findViewById(R.id.btnDividir);
+        Button btnDividir = (Button) findViewById(R.id.btnDividir);
         btnDividir.setOnClickListener(this);
 
     }
 
 
-    public void sumar(View view){
+    public void operar(View view) {
+        int op1 = 0, op2 = 0;
         try {
-            Integer res = Integer.parseInt(txtOp1.getText().toString()) + Integer.parseInt(txtOp2.getText().toString());
-            txtRespuesta.setText(res+"");
-            Toast.makeText(this, "La suma es: "+res, Toast.LENGTH_SHORT).show();
-
-        }catch (Exception e){
-            System.out.println(e);
+            op1 = Integer.parseInt(txtOp1.getText().toString());
+            op2 = Integer.parseInt(txtOp2.getText().toString());
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Por favor ingrese números válidos", Toast.LENGTH_SHORT).show();
+            return;
         }
-
+        int res = 0;
+        switch (view.getId()) {
+            case R.id.btnSumar:
+                res = op1 + op2;
+                Toast.makeText(this, "La suma es: " + res, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnRestar:
+                estar:
+                res = op1 - op2;
+                Toast.makeText(this, "La resta es: " + res, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnMultiplicar:
+                res = op1 * op2;
+                Toast.makeText(this, "La multiplicación es: " + res, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnDividir:
+                if (op2 == 0) {
+                    Toast.makeText(this, "No se puede dividir por cero", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                res = op1 / op2;
+                Toast.makeText(this, "La división es: " + res, Toast.LENGTH_SHORT).show();
+                break;
+        }
+        txtRespuesta.setText(Integer.toString(res));
     }
-
-    public void restar(View view){
-        try {
-            Integer res = Integer.parseInt(txtOp1.getText().toString()) - Integer.parseInt(txtOp2.getText().toString());
-            txtRespuesta.setText(res+"");
-            Toast.makeText(this, "La resta es: "+res, Toast.LENGTH_SHORT).show();
-
-        }catch (Exception e){
-            System.out.println(e);
-        }
-       }
-
-    public void multiplicar(View view){
-        try {
-            Integer res = Integer.parseInt(txtOp1.getText().toString()) * Integer.parseInt(txtOp2.getText().toString());
-            txtRespuesta.setText(res+"");
-            Toast.makeText(this, "La multiplicacion es: "+res, Toast.LENGTH_SHORT).show();
-
-        }catch (Exception e){
-            System.out.println(e);
-        }
-          }
-
-    public void dividir(View view){
-        try {
-            Integer res = Integer.parseInt(txtOp1.getText().toString()) / Integer.parseInt(txtOp2.getText().toString());
-            txtRespuesta.setText(res+"");
-            Toast.makeText(this, "La division es: "+res, Toast.LENGTH_SHORT).show();
-
-        }catch (Exception e){
-            System.out.println(e);
-        }
-    }
-
 
 
     @Override
@@ -97,17 +86,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Vibrator vibrador = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         vibrador.vibrate(VibrationEffect.createOneShot(150, VibrationEffect.DEFAULT_AMPLITUDE));
 
-        if(view.getId() == R.id.btnSumar){
-            sumar(view);
-
-        } else if(view.getId() == R.id.btnRestar){
-            restar(view);
-
-        } else if(view.getId() == R.id.btnMultiplicar){
-            multiplicar(view);
-
-        }else if(view.getId() == R.id.btnDividir){
-            dividir(view);
-        }
+        operar(view);
     }
 }
